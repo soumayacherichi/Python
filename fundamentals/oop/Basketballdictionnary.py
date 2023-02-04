8a5a9a242481dd03d73b13a0351bec873d8cbdeb
Soumaya Cherichi <soumayacherichi@gmail.com>
2023-02-04 20:08:48 +0100

    oop

---
+++ fundamentals/oop/Basketballdictionnary.py
@@ -0,0 +1,39 @@
+class Player:
+        def __init__(self, data):
+                self.name = data['name']
+                self.age = data ['age']
+                self.position = data['position']
+                self.team = data['team']
+        def __repr__(self):
+                display = f"Player: {self.name}, {self.age} y/o, pos: {self.position}, team: {self.team}"
+                return display
+        @classmethod
+        def add_players(cls, data):
+                player_objects = []
+                for dict in data:
+                        player_objects.append(cls(dict))
+                return player_objects
+kevin = {
+        "name": "Kevin Durant", 
+        "age":34, 
+        "position": "small forward", 
+        "team": "Brooklyn Nets"
+}
+jason = {
+        "name": "Jason Tatum", 
+        "age":24, 
+        "position": "small forward", 
+        "team": "Boston Celtics"
+}
+kyrie = {
+        "name": "Kyrie Irving", 
+        "age":32, "position": "Point Guard", 
+        "team": "Brooklyn Nets"
+}  
+# Create your Player instances here!
+player_jason = Player(jason)
+player_kevin = Player(kevin)
+player_kyrie = Player(kyrie)
+print(player_jason)
+print(player_kevin)
+print(player_kyrie)
